16ff91b2a4627493448b75ffb00edba46852422a
Peter Csajtai <peter.csajtai@logmein.com>
2017-05-05 16:42:18 +0200

    Fixed assembly title

--- src/stashbox.signalr/Properties/AssemblyInfo.cs
+++ src/stashbox.signalr/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Stashbox.SignalR")]
+[assembly: AssemblyTitle("Stashbox.AspNet.SignalR")]
 [assembly: AssemblyDescription("ASP.NET SignalR integration for Stashbox")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Peter Csajtai")]
-[assembly: AssemblyProduct("Stashbox.SignalR")]
+[assembly: AssemblyProduct("Stashbox.AspNet.SignalR")]
 [assembly: AssemblyCopyright("Copyright © Peter Csajtai 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
